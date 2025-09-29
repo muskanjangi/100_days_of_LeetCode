517c948022fd9557eabe6753063b8910b257abb1
muskanjangi <163982489+muskanjangi@users.noreply.github.com>
2025-09-29 20:09:23 +0530

    Fix: remove submodule and restore Day_1 as regular folder

---
+++ Day_15/sol15.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen; 
+
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            if (seen.count(complement)) {
+                return {seen[complement], i};
+            }
+            seen[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> result = sol.twoSum(nums, target);
+    cout << "Indices: ";
+    for (int idx : result) {
+        cout << idx << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
