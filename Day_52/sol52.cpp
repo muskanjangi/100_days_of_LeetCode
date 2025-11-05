cb81552930cb07eccd09df58c84c1380f0bbb5d1
muskanjangi <163982489+muskanjangi@users.noreply.github.com>
2025-11-05 20:28:37 +0530

    Add Day_52 Solutions

---
+++ Day_52/sol52.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return low; //if target would be less or more the low will return the index bcz if something wants to get insert so,it inserts from low not from high
+    }
+};
