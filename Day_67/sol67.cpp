64089d4904902cb7b625ed4924b317cb74f88782
muskanjangi <163982489+muskanjangi@users.noreply.github.com>
2025-11-20 19:22:48 +0530

    Add Day_67 Solutions

---
+++ Day_67/sol67.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int row = 0;
+        int col = n - 1;
+        while (row < m && col >= 0) {
+            if (matrix[row][col] == target) return true;
+            else if (matrix[row][col] < target) row++;
+            else col--;
+        }
+        return false;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<vector<int>> matrix = {
+        {1, 4, 7, 11, 15},
+        {2, 5, 8, 12, 19},
+        {3, 6, 9, 16, 22},
+        {10, 13, 14, 17, 24},
+        {18, 21, 23, 26, 30}
+    };
+    int target = 5;
+
+    if (sol.searchMatrix(matrix, target))
+        cout << "Target found!" << endl;
+    else
+        cout << "Target not found." << endl;
+
+    return 0;
+}
